cf8e8d53b2a90dc07a7516848b501eb1d09d6541
BPilecarte <beatrizpilecart3@gmail.com>
2023-09-18 22:47:02 -0300

    Configuração do Picker para selecionar o tipo de dado e imagem correspondentes

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -4,64 +4,61 @@ namespace DiceRoller;
 
 public partial class MainPage : ContentPage
 {
-    int numSides = 6;
-
-    public MainPage()
+	public MainPage()
 	{
 		InitializeComponent();
 		sidesPicker.SelectedIndex = 0;
-    }
+	}
 
 	public class Dice
 	{
-		//public int QuantidadeDeLados { get; set; }
+		//numSides - atributo da classe
+		private int numSides;
 
+		//construtor
 		public Dice() { }
 
+		//construtor com parametro
 		public Dice(int numSides)
 		{
 			this.numSides = numSides;
 		}
 
-		private int numSides;
-
+		//metodo da classe
 		public int RollDice()
 		{
-			int radom = new Random().Next(1, numSides +1);
+			int radom = new Random().Next(1, numSides + 1);
 			return radom;
+			//ou
+			//Random random = new Random();
+			//int numAleatorio = random.Next(1, numSides + 1);
+			//return numAleatorio
+			//ou
+			//return new Random().Next(1, numSides +1);
 		}
+	}
+
+	public void OnChangedOption(object sender, EventArgs e)
+	{
+		int numSide = (int)sidesPicker.SelectedItem;
+        Image image = new Image { Source = $"dice_{numSide}.png" };
+		DiceImage.Source = image.Source;
 	}
 
 	public void btnRandom_Click(object sender, EventArgs e)
 	{
-		numSides = Convert.ToInt32(sidesPicker.SelectedItem.ToString());
-		Dice dice = new Dice(); //os numéros do picker deve ficar dentro do ()
-        NumberOfDice.Text = dice.RollDice().ToString();
+		//ler o numero que foi selecionado no picker. Criação da variável selectedSidesNumber
+		int selectedSidesNumber = (int)sidesPicker.SelectedItem;
 
+		//chamar o metodo da classe dados(Dice) para sortear o número
+		Dice dice = new Dice(selectedSidesNumber);
 
-    }
+		//chamar o método para rodar o dado
+		int NumSorteado = dice.RollDice();
 
-
-
-
-
-
-
-
-
-
-	//private void btnRandom_Click(object sender, EventArgs e)
-	//{
-		//var maxValue = sidesPicker.SelectedItem; 
-		
-		//var numeroSorteado = new Random().Next(1, (int)maxValue);
-
-		//DiceOfNumber. Text = numeroSorteado.ToString();
-    //}
-
-
-
-
+		//mostrar o número sorteado na Label.   .Text irá mudar o texto(o retorno) da Label
+		NumberOfDice.Text = NumSorteado.ToString();
+	}
 
 }
 
